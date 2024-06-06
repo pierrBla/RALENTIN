c662d205b24588f60072036babb9e2193894f452
pierrBla <pierrcalderon6@hotmail.com>
2024-06-05 22:06:11 -0500

    create database Entity paises

---
+++ Ralentin/Ralentin.Backend/Data/DataContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Ralentin.Shared.Entities;
+
+namespace Ralentin.Backend.Data
+{
+    public class DataContext:DbContext
+    {
+        public DataContext(DbContextOptions<DataContext>options):base(options) 
+        {
+            
+        }
+        public DbSet <Country>Countries { get; set; }
+
+        //para que se unico  nombre un indece
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Country>().HasIndex(x => x.Name).IsUnique();
+        }
+    }
+}
---
+++ Ralentin/Ralentin.Shared/Entities/Country.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Ralentin.Shared.Entities
+{
+    public class Country
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Pais")]
+        [Required(ErrorMessage = "El  Campo {0} es Requerido")]
+
+        [MaxLength(100, ErrorMessage = "El Campo {0} no puede tener mas de {1} caracteres ")]
+        public string Name { get; set; } = null!;
+    }
+}
